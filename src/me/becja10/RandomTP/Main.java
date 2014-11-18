94c0acb78fe08304a840cd35b1d5744d6f03503e
James <10becja@gmail.com>
2014-11-18 14:55:30 -0600

    Upload

---
+++ src/me/becja10/RandomTP/Main.java
@@ -0,0 +1,291 @@
+package me.becja10.RandomTP;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	private static Main plugin;
+	private static final int MAX_BORDER = 500000;
+	private static Random gen;
+	private static final int SPINNER = 100;
+	private static boolean gotWorld = true;
+	
+	private static Location spawn;
+	private static World world;
+	
+	@Override
+	public void onDisable(){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+	}
+	
+	@Override
+	public void onEnable(){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Version "+ pdfFile.getVersion() + " Has Been Enabled!");
+	    getServer().getPluginManager().registerEvents(this, this); //register events
+		plugin = this;
+		gen = new Random();
+		gen.setSeed(System.currentTimeMillis());
+		
+	    //Save the files
+	    saveDefaultConfig(); //fail silently if config already exists
+	    FileManager.saveDefaultPlayers();
+	    
+	    world = Bukkit.getWorld(plugin.getConfig().getString("World"));
+	    //make sure a valid world is loaded
+	    if(world == null)
+	    {
+			this.logger.info(pdfFile.getName() + " The world specified in RandomTP/config does not exist");
+			this.logger.info(pdfFile.getName() + " Will not check for first time joiners to teleport");
+			gotWorld = false;
+	    }
+	    else
+    	{
+	    	//it hasn't been configured yet
+	    	if(plugin.getConfig().getString("Spawn").equalsIgnoreCase("reset"))
+	    	{
+	    		setSpawn(world.getSpawnLocation());
+	    		plugin.getConfig().set("Spawn", null);
+	    	}
+	    	else
+	    		spawn = new Location(world, plugin.getConfig().getInt("Spawn.x"),
+	    									plugin.getConfig().getInt("Spawn.y"),
+	    									plugin.getConfig().getInt("Spawn.z"));
+    	}    
+	}
+	
+	/*
+	 * If the player is joining for the first time, randomly teleport them somewhere
+	 */
+	@EventHandler
+	public void onChangeWorld(PlayerChangedWorldEvent e)
+	{
+		//make sure we have a world
+		if(!gotWorld) return;
+		Player p = e.getPlayer();
+		World w = p.getWorld();
+		String id = p.getUniqueId().toString();
+				
+		//see if we care about this world
+		if (world.equals(w))
+		{
+			//check to see if the player has joined this world before
+			if (!FileManager.getPlayers().contains(w.getName()+"."+id)) 
+			{
+				//    world:
+				//      UUID: name
+				FileManager.getPlayers().set(w.getName()+"."+id, p.getName());
+				FileManager.savePlayers();
+				//do the teleport
+				doTeleport(p,w);
+			}
+		}
+	}
+	
+	/*
+	 * Helper function to actually do the teleport. YAY refactoring
+	 */
+	private void doTeleport(Player p, World w)
+	{
+		//just to make sure we don't teleport them somewhere bad, ie over lava...
+		boolean successful = false;
+		int count = 0;
+		while(!successful & count < SPINNER)
+		{
+			successful = randomTeleport(p, w);
+			count++; //just to make sure we don't spin indefinitely
+		}
+		
+		//let people know what did/n't happen
+		if(successful)
+		{
+			this.logger.info("[RandomTP] Successfully teleported "+p.getName());
+			p.sendMessage(ChatColor.BLUE + "[RandomTP]"+ChatColor.GREEN+" You have been randomly teleported!");
+		}
+		else
+		{
+			this.logger.info("[RandomTP] Failed to teleport "+p.getName());
+			p.sendMessage(ChatColor.BLUE + "[RandomTP]"+ChatColor.RED+" Random teleportation failed!");
+		}
+	}
+	
+	/*
+	 * Teleports the player somewhere random in the world, checking for claims
+	 */
+	private boolean randomTeleport(Player player, World world)
+	{
+		//just as sanity check, in case for some reason my logic breaks
+		Location possible = spawn;
+		
+		//how far away the can be teleported to
+		int border = plugin.getConfig().getInt("Border");
+		if(border == -1)
+			border = MAX_BORDER;
+		
+		boolean foundSpot = false;
+		//keep spinning until we find an unclaimed place to teleport them too
+		int spawnX = (int)spawn.getX();
+		int spawnZ = (int)spawn.getZ();
+		int count = 0;
+		while(!foundSpot & count < SPINNER)
+		{
+			int x = getRandomCoord(spawnX - border + 10, spawnX + border - 10);
+			int z = getRandomCoord(spawnZ - border + 10, spawnZ + border - 10);
+			int y = world.getHighestBlockYAt(x, z);
+			possible = new Location(world, x,y,z);
+			foundSpot = checkLocation(possible);
+			count++; //make sure that it stops at some point...
+		}
+		if(foundSpot) //if we found a spot and didn't run out of spinning
+			return (foundSpot & player.teleport(possible));
+		return false;
+	}
+	
+	/*
+	 * checks whether or not this is unclaimed
+	 */
+	private boolean checkLocation(Location l)
+	{
+		Claim claim = GriefPrevention.instance.dataStore.getClaimAt(l, true, null);
+		
+		//if we've landed in a claim, that's bad
+		if(claim != null) return false;
+		
+		//else, see if there's a claim nearby
+		for(int x = -10; x <= 10; x++)
+			for(int z = -10; z <= 10; z++)
+			{
+				if(GriefPrevention.instance.dataStore.getClaimAt(new Location(l.getWorld(), (int)l.getX()+ x, 65, (int)l.getZ()+ z),true, null) != null)
+					return false;
+			}
+		
+		//otherwise there aren't any claims nearby, and this is a valid location
+		return true;		
+	}
+	
+	/*
+	 * Generates a random location
+	 */
+	private int getRandomCoord(int min, int max)
+	{		
+		return gen.nextInt((max - min)+1) + min;		
+	}
+	
+	/*
+	 * To manually teleport someone randomly
+	 */
+	@SuppressWarnings("deprecation")
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+	{
+		//TPR
+		if(cmd.getName().equalsIgnoreCase("tpr"))
+		{
+			boolean isPlayer = sender instanceof Player;
+			switch (args.length)
+			{
+				case 0:
+					if(isPlayer)
+						tpr((Player) sender, null);
+					else //console
+						sender.sendMessage("This command can only be run by a player.");
+					break;
+				case 1:
+					Player t = Bukkit.getPlayer(args[0]);
+					if(t == null)
+						sender.sendMessage(ChatColor.BLUE+"[RandomTP]"+ChatColor.RED+"Player not found.");
+					else 
+						if(isPlayer)
+							tpr((Player) sender, t);
+						else //console, no need to check for permission
+							doTeleport(t, t.getWorld());
+					break;
+				default: //greater than 1
+					sender.sendMessage(ChatColor.BLUE+"[RandomTP]"+ChatColor.RED+"Wrong usage. Try /tpr <player>");
+					break;					
+			}
+			return true;
+		}
+		//TPRS
+		else if(cmd.getName().equalsIgnoreCase("tprs"))
+		{
+			if (!(sender instanceof Player))
+				sender.sendMessage("This command can only be run by a player.");
+			else 
+			{
+				Player p = (Player) sender;
+				if(!(p.hasPermission("randomtp.admin")))
+					p.sendMessage(ChatColor.DARK_RED+"You do not have permission to use this command!");
+				else
+				{
+					if(p.getWorld().equals(world))
+					{
+						setSpawn(p.getLocation());
+						p.sendMessage(ChatColor.BLUE + "[RandomTP]"+ChatColor.GREEN+" Starting point set!");
+					}
+					else
+						p.sendMessage(ChatColor.BLUE + "[RandomTP]"+ChatColor.RED+" Wrong world!");
+
+				}
+			}
+			return true;
+		}			
+		return false;
+	}
+	
+	/*
+	 * tpr command
+	 */
+	private void tpr(Player p, Player target)
+	{
+		//no target, therefore just check player
+		if(target == null)
+		{
+			if(!(p.hasPermission("randomtp.player")))
+				p.sendMessage(ChatColor.DARK_RED+"You do not have permission to use this command!");
+			else
+				doTeleport(p, p.getWorld());
+		}
+		else
+		{
+			if(!(p.hasPermission("randomtp.admin")))
+				p.sendMessage(ChatColor.DARK_RED+"You do not have permission to use this command!");
+			else
+				doTeleport(target, target.getWorld());
+		}
+	}
+	
+	/*
+	 * tprspawn command
+	 */
+	private void setSpawn(Location l)
+	{	
+		plugin.getConfig().set("Spawn.x", l.getBlockX());
+		plugin.getConfig().set("Spawn.y", l.getBlockY());
+		plugin.getConfig().set("Spawn.z", l.getBlockZ());
+		spawn = l;
+		plugin.saveConfig();
+	}
+	
+	public static Main getInstance()
+	{
+		return plugin;
+	}
+}
